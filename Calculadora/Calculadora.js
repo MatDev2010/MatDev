9780bf9f0e1a2feacbc2838f3c1d4f653f43f5f9
MatDev2010 <96796792+MatDev2010@users.noreply.github.com>
2022-01-01 20:17:18 -0300

    Alterações

--- Calculadora/Calculadora.js
+++ Calculadora/Calculadora.js
@@ -1,5 +1,6 @@
 var calculo = ''
-var calculo2 = ''
+var fator1 = 0
+var fator2 = 0
 
 function clique(tecla) {
     calculo += String(tecla[5])
@@ -7,6 +8,17 @@ function clique(tecla) {
 
 }   
 
-function cont() {
-    
+function cont(tipo) {
+    if (tipo == 'adicao') {
+        fator1 = Number(calculo)
+        calculo += ' + '
+        document.getElementById("calc").innerHTML = calculo
+    }
+}
+
+function reset() {
+    calculo = ''
+    fator1 = 0
+    fator2 = 0
+    document.getElementById("calc").innerHTML = 'Conta'
 }
