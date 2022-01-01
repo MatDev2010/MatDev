5d691d58813dbdecfaf121baf69e53c81f3a7b5c
MatDev2010 <96796792+MatDev2010@users.noreply.github.com>
2022-01-01 20:07:09 -0300

    Atualizações no Calculadora (ainda não está pront)

---
+++ Calculadora/Calculadora.js
@@ -0,0 +1,12 @@
+var calculo = ''
+var calculo2 = ''
+
+function clique(tecla) {
+    calculo += String(tecla[5])
+    document.getElementById("calc").innerHTML = calculo
+
+}   
+
+function cont() {
+    
+}
