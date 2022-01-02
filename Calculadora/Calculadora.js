5d46f86d881ca8ef68537080a6328fd32ee6d2c6
MatDev2010 <96796792+MatDev2010@users.noreply.github.com>
2022-01-01 21:11:27 -0300

    Calculadora

--- Calculadora/Calculadora.js
+++ Calculadora/Calculadora.js
@@ -1,8 +1,15 @@
 var calculo = ''
-var fator1 = 0
-var fator2 = 0
+var elem1 = 0
+var elem2 = 0
+var limpar = false
 
 function clique(tecla) {
+
+    if (limpar) {
+        calculo = ''
+        limpar = !limpar
+    }
+
     calculo += String(tecla[5])
     document.getElementById("calc").innerHTML = calculo
 
@@ -10,15 +17,49 @@ function clique(tecla) {
 
 function cont(tipo) {
     if (tipo == 'adicao') {
-        fator1 = Number(calculo)
         calculo += ' + '
         document.getElementById("calc").innerHTML = calculo
+    }
+    else if (tipo == 'subtracao') {
+        calculo += ' - '
+        document.getElementById("calc").innerHTML = calculo
+    }
+    else if (tipo == 'divisao') {
+        calculo += ' ÷ '
+        document.getElementById("calc").innerHTML = calculo
+    }
+    else if (tipo == 'vezes') {
+        calculo += ' x '
+        document.getElementById("calc").innerHTML = calculo
+    }
+    else if (tipo == 'igual') {
+        var calcular = calculo.split(' ')
+        console.log(calcular)
+        var resultado = Number(calcular[0])
+       
+        for (let i = 0; i <= calcular.length; i++) {
+             if (calcular[i] == '+') {
+                resultado += Number(calcular[i + 1])
+                // console.log(i, typeof i, calcular[(i + 1)], resultado)
+            }
+            else if (calcular[i] == '-') {
+                resultado -= Number(calcular[i + 1])
+            }
+            else if (calcular[i] == 'x') {
+                resultado *= Number(calcular[i + 1])
+            }
+            else if (calcular[i] == '÷') {
+                resultado /= Number(calcular[i + 1])
+            } 
+        }
+        document.getElementById("calc").innerHTML = calculo += ' = ' + resultado
+        limpar = true
     }
 }
 
 function reset() {
     calculo = ''
-    fator1 = 0
-    fator2 = 0
+    elem1 = 0
+    elem2 = 0
     document.getElementById("calc").innerHTML = 'Conta'
 }
