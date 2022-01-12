121dd7365d80a2d2a51bee69c45dd2e3cc81c3da
Adler Cossovan <adler.cossovan@gmail.com>
2022-01-12 18:59:45 -0300

    ajuste

--- Enigma/Fase1/Fase.js
+++ Enigma/Fase1/Fase.js
@@ -1,10 +1,10 @@
 function resposta() {
     let resposta = document.getElementById("resposta").value
     if (resposta == "Ilusao") {
-        document.getElementById("textoresposta").value = "Correto!"
         setInterval(() =>
-        window.location.replace("http://127.0.0.1:5500/Enigma/Fase2/index.html")
+        window.open("http://127.0.0.1:5500/Enigma/Fase2/indexFase2.html")
         , 2000)
+        document.getElementById("textoresposta").innerHTML = "Correto!"
     } else {
         document.getElementById("textoresposta").innerHTML = "Errado"
     }
