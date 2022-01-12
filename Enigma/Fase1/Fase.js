51a223a1a3783fde8e5609597142f1e9f4217022
MatDev2010 <96796792+MatDev2010@users.noreply.github.com>
2022-01-12 16:36:11 -0300

    enigma

--- Enigma/Fase1/Fase.js
+++ Enigma/Fase1/Fase.js
@@ -1,7 +1,9 @@
 function resposta() {
     let resposta = document.getElementById("resposta").value
+    console.log(resposta)
     if (resposta == "Ilusao") {
         document.getElementById("resposta").value = "Correto"
+    } else {
+        document.getElementById("textoresposta").innerHTML = "Errado"
     }
-    document.getElementById("resposta").value = "Errado"
 }
